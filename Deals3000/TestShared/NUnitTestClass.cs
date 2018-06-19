d7cc9e118ad8c39c6ccadc9d3574e7546adf306d
hla <henri.la@entwicklungspark.com>
2018-06-19 22:32:57 +0200

    TestShared project

---
+++ Deals3000/TestShared/NUnitTestClass.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using Deals3000.Models;
+using Deals3000.Services;
+
+namespace TestShared
+{
+    [TestFixture()]
+    public class NUnitTestClass
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+
+        [Test]
+        public void Fail(){
+            MockDataStore mockDataStore = new MockDataStore();
+            var products = mockDataStore.GetItemsAsync(true).Result;
+            List<Product> _products = (List<Product>) products;
+            Console.WriteLine($"##### -> {_products[0].Name}");
+        }
+    }
+}
